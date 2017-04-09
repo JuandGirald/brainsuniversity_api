e02be926209ede095d3e83d7b1bcac718b108878
kevanz10 <krojas1171@gmail.com>
2017-04-08 21:14:36 -0500

    fixed indentation problems

--- app/controllers/api/v1/students/schedules_controller.rb
+++ app/controllers/api/v1/students/schedules_controller.rb
@@ -1,49 +1,49 @@
 module Api::V1
-	class Students::SchedulesController < ApiController
-		load_and_authorize_resource
+  class Students::SchedulesController < ApiController
+    load_and_authorize_resource
 
-		before_action :set_schedule, only: [:update, :show]
+    before_action :set_schedule, only: [:update, :show]
 
-		def index
-			page = params[:page].present? ? params[:page] : 1 
-			if params[:status].present?
-				@schedules = current_user.schedules.unscoped.send(params[:status])
-			else
-				@schedules = current_user.schedules
-			end
-			render json: @schedules.paginate(page: page, :per_page => 10), each_serializer: ListSchedulesSerializer
-		end
+    def index
+      page = params[:page].present? ? params[:page] : 1 
+      if params[:status].present?
+        @schedules = current_user.schedules.unscoped.send(params[:status])
+      else
+        @schedules = current_user.schedules
+      end
+      render json: @schedules.paginate(page: page, :per_page => 10), each_serializer: ListSchedulesSerializer
+    end
 
-		def show
-			render json: @schedule, serializer: ScheduleSerializer
-		end
+    def show
+      render json: @schedule, serializer: ScheduleSerializer
+    end
 
-		def create
-			@user = current_user.schedules.create(schedule_params)
-			if @user.save
-				render json: @user, serializer: ScheduleSerializer
-			else
-			  render json: @user.errors, status: :unprocessable_entity
-			end
-		end
+    def create
+      @user = current_user.schedules.create(schedule_params)
+      if @user.save
+        render json: @user, serializer: ScheduleSerializer
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
+    end
 
-		def update
-			if @schedule.update_attributes(schedule_params)
-				render json: @schedule, serializer: ScheduleSerializer
-	    else
-	      render json: @schedule.errors, status: :unprocessable_entity
-	    end
-		end
+    def update
+      if @schedule.update_attributes(schedule_params)
+        render json: @schedule, serializer: ScheduleSerializer
+      else
+        render json: @schedule.errors, status: :unprocessable_entity
+      end
+    end
 
-		private
-			def schedule_params
-				params.require(:schedule).permit(:id, :start_at, :end_at, 
-																				 :status, :duration, :modality, 
-																				 :student_id, :teacher_id)
-			end
+    private
+      def schedule_params
+        params.require(:schedule).permit(:id, :start_at, :end_at, 
+                                         :status, :duration, :modality, 
+                                         :student_id, :teacher_id)
+      end
 
-			def set_schedule
-				@schedule = current_user.schedules.find(params[:id])
-			end
-	end
+      def set_schedule
+        @schedule = current_user.schedules.find(params[:id])
+      end
+  end
 end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,35 +1,35 @@
 class Schedule < ApplicationRecord
-	before_create :set_default_status 
-	before_validation :set_free_duration
+  before_create :set_default_status 
+  before_validation :set_free_duration
 
-	belongs_to :teacher, inverse_of: :schedules
-	delegate :email, to: :teacher, prefix: true
+  belongs_to :teacher, inverse_of: :schedules
+  delegate :email, to: :teacher, prefix: true
 
-	belongs_to :student, inverse_of: :schedules
-	has_one :room, inverse_of: :schedule
-	delegate :email, to: :student, prefix: true
+  belongs_to :student, inverse_of: :schedules
+  has_one :room, inverse_of: :schedule
+  delegate :email, to: :student, prefix: true
 
-	validates :teacher, :student, presence: true
-	validates :start_at, :duration, presence: true
+  validates :teacher, :student, presence: true
+  validates :start_at, :duration, presence: true
 
-	enum status: { awaiting_tutor: '1', 
-								 accepted_awaiting_payment: '2',
-               	 confirmed: '3',
-               	 rejected: '4',
-               	 expired: '5',
-               	 canceled: '6',
-               	 completed: '7'
-							 }
+  enum status: { awaiting_tutor: '1', 
+                 accepted_awaiting_payment: '2',
+                 confirmed: '3',
+                 rejected: '4',
+                 expired: '5',
+                 canceled: '6',
+                 completed: '7'
+               }
+  
+  default_scope { where status: 'awaiting_tutor' }
+  
+  private
 
-	default_scope { where status: 'awaiting_tutor' }
-	
-	private
+    def set_default_status
+      self.status = 'awaiting_tutor'
+    end
 
-		def set_default_status
-			self.status = 'awaiting_tutor'
-		end
-
-		def set_free_duration
-			self.duration = 15.minutes if modality == 'free'
-		end
+    def set_free_duration
+      self.duration = 15.minutes if modality == 'free'
+    end
 end
