2270eda74c78964886a2c35055fd746103c97129
Juan David Girlado Rodriguez <jdgiraldo88@hotmail.com>
2017-04-09 12:37:57 -0500

    fix identation

--- app/controllers/api/v1/teachers/schedules_controller.rb
+++ app/controllers/api/v1/teachers/schedules_controller.rb
@@ -1,11 +1,11 @@
 module Api::V1
-	class Teachers::SchedulesController < ApiController
-		load_and_authorize_resource
+  class Teachers::SchedulesController < ApiController
+    load_and_authorize_resource
 
-		before_action :set_schedule, only: [:update, :show]
+    before_action :set_schedule, only: [:update, :show]
 
-		def index
-			page = params[:page].present? ? params[:page] : 1 
+    def index
+      page = params[:page].present? ? params[:page] : 1 
       if params[:status].present?
         @schedules = current_user.schedules.send(params[:status])
       else
@@ -14,27 +14,27 @@ module Api::V1
       render json: @schedules.paginate(page: page, :per_page => 10), each_serializer: ListSchedulesSerializer
     end
 
-		def show
-			render json: @schedule, serializer: ScheduleSerializer
-		end
+    def show
+      render json: @schedule, serializer: ScheduleSerializer
+    end
 
-		def update
-			if @schedule.update_attributes(schedule_params)
-				render json: @schedule, serializer: ScheduleSerializer
-	    else
-	      render json: @schedule.errors, status: :unprocessable_entity
-	    end
-		end
+    def update
+      if @schedule.update_attributes(schedule_params)
+        render json: @schedule, serializer: ScheduleSerializer
+      else
+        render json: @schedule.errors, status: :unprocessable_entity
+      end
+    end
 
-		private
-			def schedule_params
-				params.require(:schedule).permit(:id, :start_at, :end_at, 
-																				 :status, :duration, :modality, 
-																				 :student_id, :teacher_id)
-			end
+    private
+      def schedule_params
+        params.require(:schedule).permit(:id, :start_at, :end_at, 
+                                         :status, :duration, :modality, 
+                                         :student_id, :teacher_id)
+      end
 
-			def set_schedule
-				@schedule = current_user.schedules.find(params[:id])
-			end
-	end
+      def set_schedule
+        @schedule = current_user.schedules.find(params[:id])
+      end
+  end
 end
