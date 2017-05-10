1bf22a304e3aa0f32d61461c5dd60ff1562a8502
Juan David Girlado Rodriguez <jdgiraldo88@hotmail.com>
2017-05-09 19:11:56 -0500

    Parse availability array params to store it

--- app/controllers/api/v1/teachers_controller.rb
+++ app/controllers/api/v1/teachers_controller.rb
@@ -39,6 +39,7 @@ module Api::V1
     # PATCH/PUT /teachers/1
     def update
       if @user.update(teacher_params)
+        @user.parse_availability_params(params["teacher"]["availability_attributes"])
         @user.update_subjects(params["teacher"]["subjects"]) if params["teacher"]["subjects"].present?
         @user.schedule_step if @user.pending?
         render json: @user, serializer: UserProfileSerializer
