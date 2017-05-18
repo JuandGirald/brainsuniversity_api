2326f31f731eee3ac4d820e25aa035d3546729c1
Juan David Girlado Rodriguez <jdgiraldo88@hotmail.com>
2017-05-18 14:46:38 -0500

    Fix limit for integers in bank information

--- app/controllers/api/v1/teachers/bank_informations_controller.rb
+++ app/controllers/api/v1/teachers/bank_informations_controller.rb
@@ -1,38 +1,38 @@
 module Api::V1
-	class Teachers::BankInformationsController < ApiController
-		include ErrorSerializer
-		load_and_authorize_resource
-		load_and_authorize_resource :bank_information, :through => :teacher
+  class Teachers::BankInformationsController < ApiController
+    include ErrorSerializer
+    load_and_authorize_resource
+    load_and_authorize_resource :bank_information, :through => :teacher
 
-		before_action :set_user, :set_bank_information
+    before_action :set_user, :set_bank_information
 
-		def show
-			render json: @bank_information, serializer: BankSerializer
-		end
+    def show
+      render json: @bank_information, serializer: BankSerializer
+    end
 
-		def update
-			@bank_information = @user.bank_information
-			if @bank_information.update_attributes(bank_information_params)
-				@user.complete!
-	      render json: @bank_information, serializer: BankSerializer
-	    else
-	      render json: ErrorSerializer.serialize(@user.errors), status: :unprocessable_entity
-	    end
-		end
+    def update
+      @bank_information = @user.bank_information
+      if @bank_information.update_attributes(bank_information_params)
+        @user.complete!
+        render json: @bank_information, serializer: BankSerializer
+      else
+        render json: ErrorSerializer.serialize(@user.bank_information.errors), status: :unprocessable_entity
+      end
+    end
 
-		private
-			def set_user
-				@user = current_user
-			end
+    private
+      def set_user
+        @user = current_user
+      end
 
-			def set_bank_information
-				@bank_information = BankInformation.find(params[:id])
-			end
+      def set_bank_information
+        @bank_information = BankInformation.find(params[:id])
+      end
 
-			def bank_information_params
-				params.require(:bank_information).permit(:id, :owner_id, :account_number, 
-																								:bank_name, :account_type, 
-																								:owner_name, :teacher_id)
-			end
-	end
+      def bank_information_params
+        params.require(:bank_information).permit(:id, :owner_id, :account_number, 
+                                                :bank_name, :account_type, 
+                                                :owner_name, :teacher_id)
+      end
+  end
 end
--- app/models/bank_information.rb
+++ app/models/bank_information.rb
@@ -2,7 +2,7 @@ class BankInformation < ApplicationRecord
 	belongs_to :teacher, dependent: :destroy
 	validates :owner_id, :account_number, :bank_name, :account_type, 
 						:owner_name, presence: true, on: [:update]
-	validates_length_of :account_number, is: 11, message: 'Debe ser de 11 digitos',
+	validates_inclusion_of :account_number, in: 0..99999999999, message: 'Debe ser de 11 digitos',
 											on: [:update]	
 	validate :teacher					
 end
