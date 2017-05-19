0274fde2af05b931494cd8163dae4fb70f81b73b
Juan David Girlado Rodriguez <jdgiraldo88@hotmail.com>
2017-05-19 01:04:43 -0500

    show profile data in index teacher enpoint

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :role, :activated, :first_name, :last_name,
   						:created_at, :updated_at, :subjects
+
+  has_one :profile
 end
