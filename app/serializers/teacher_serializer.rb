988d3ad2c24e6a10f0500c4cb608b9451651d4ba
Juan David Girlado Rodriguez <jdgiraldo88@hotmail.com>
2017-05-21 21:43:46 -0500

    Revert teacher serializer

--- app/serializers/teacher_serializer.rb
+++
@@ -1,7 +0,0 @@
-class TeacherSerializer < ActiveModel::Serializer
-  attributes :id, :email, :role, :activated, :first_name, :last_name,
-              :created_at, :updated_at, :subjects
-
-  has_one :profile
-  has_one :availability
-end
