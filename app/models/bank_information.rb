43c87c8cb37e96ec7531585a09e9ddac2949d351
Juan David Girlado Rodriguez <jdgiraldo88@hotmail.com>
2017-06-01 17:29:34 -0500

    Change limit of digits for bank information

--- app/models/bank_information.rb
+++ app/models/bank_information.rb
@@ -2,7 +2,7 @@ class BankInformation < ApplicationRecord
 	belongs_to :teacher, dependent: :destroy
 	validates :owner_id, :account_number, :bank_name, :account_type, 
 						:owner_name, presence: true, on: [:update]
-  validates_length_of :account_number, :minimum => 8, :maximum => 11, message: 'Debe estar entre 8 y 11 dígitos',
+  validates_length_of :account_number, :minimum => 8, :maximum => 12, message: 'Debe estar entre 8 y 12 dígitos',
                       on: [:update]             
 	validate :teacher					
 end
