4c8cfab8735ef21d54840600448939304d99e46c
Juan David Girlado Rodriguez <jdgiraldo88@hotmail.com>
2017-06-28 17:36:28 -0500

    Format chat endpoint dates

--- app/serializers/chat_serializer.rb
+++ app/serializers/chat_serializer.rb
@@ -1,27 +1,31 @@
 class ChatSerializer < ActiveModel::Serializer
-	attributes :id, :sender_id, :recipient_id, :recipient_email, :sender_email, 
-						 :sender_name, :recipient_name, :last_message, :readed, :updated_at
+  attributes :id, :sender_id, :recipient_id, :recipient_email, :sender_email, 
+             :sender_name, :recipient_name, :last_message, :readed, :updated_at
 
-	#Displays sender's message full name
-	def sender_name
-		object.sender.first_name + " " + object.sender.last_name
-	end
+  #Displays sender's message full name
+  def sender_name
+    object.sender.first_name + " " + object.sender.last_name
+  end
 
-	def sender_email
-		object.sender.email
-	end
+  def sender_email
+    object.sender.email
+  end
 
-	def recipient_email
-		object.recipient.email
-	end
+  def recipient_email
+    object.recipient.email
+  end
 
-	#Displays recipient's message full name
-	def recipient_name
-		object.recipient.first_name + " " + object.recipient.last_name
-	end
+  def updated_at
+    object.updated_at.to_formatted_s(:short)
+  end
 
-	def last_message
-		object.messages.last.body
-	end
+  #Displays recipient's message full name
+  def recipient_name
+    object.recipient.first_name + " " + object.recipient.last_name
+  end
+
+  def last_message
+    object.messages.last.body
+  end
 
 end
