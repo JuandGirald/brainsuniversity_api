cf55de38117f6240b6de4b1c8a8d34b676339eb3
Juan David Girlado Rodriguez <jdgiraldo88@hotmail.com>
2017-08-11 00:00:59 -0500

    Fix coupons valid query

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -2,7 +2,7 @@ class Coupon < ApplicationRecord
   has_many :student_coupons
   has_many :students, through: :student_coupons
 
-  scope :valid, -> { where("valid_until <= ? AND valid_until >= ?", Date.today, Date.today) }
+  scope :valid, -> { where("valid_from <= ? AND valid_until >= ?", Date.today, Date.today) }
 
   def valid_date?
     valid_until >= Date.today 
