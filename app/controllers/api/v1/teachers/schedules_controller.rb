c87c32b46535a41e1948a5a9d4d32e79bcd2ec29
Juan David Girlado Rodriguez <jdgiraldo88@hotmail.com>
2017-08-23 00:09:16 -0500

    Add duration to schedule session

--- app/controllers/api/v1/teachers/schedules_controller.rb
+++ app/controllers/api/v1/teachers/schedules_controller.rb
@@ -25,7 +25,8 @@ module Api::V1
                        apikey: ENV['OPENTOK_API_KEY'],
                        sessionId: @schedule.room.session_id,
                        token: @schedule.room.teacher_token,
-                       duration: @schedule.duration.to_i * 60
+                       duration: @schedule.duration.to_i * 60,
+                       start_at: @schedule.start_at
                      }
       else
         render json: { error: "Tu tutoria no estará disponible hasta el: #{@schedule.start_at.to_formatted_s(:short)}" }
